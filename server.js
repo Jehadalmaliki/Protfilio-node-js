6904b80cab2598f9dfdfe089399ef13aed611ace
Eng Jehad Almaliki <deeppp901@gmail.com>
2022-02-17 14:06:43 +0300

    Express router index

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.get('/', function(req, res) {
   res.render('pages/index');
 });
 
-  
+app.get('/index', function(req, res) {
+  res.render('pages/index');
+});  
 
 app.listen(port);
 console.log('Server is listening on port 3000');
